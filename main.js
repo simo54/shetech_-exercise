91010dd818aa789c66bcd27b784aa61110e78893
kalyEnte <me@me.com>
2021-02-19 20:12:14 +0000

    adding a form for adding new activity

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var listItems = ["Buy coffee", "Buy milk", "Disco dance"];
 // Defining our `updateList()` function...
 var updateList = function (items) {
   var listElement = document.querySelector("#task-list");
+  listElement.innerHTML = "";
 
   items.forEach(function (item) {
     listElement.innerHTML += "<li>" + item + "</li>";
@@ -10,3 +11,17 @@ var updateList = function (items) {
 };
 
 updateList(listItems);
+
+var createNew = function (event) {
+  event.preventDefault();
+
+  var newItemElement = document.querySelector("#new-item");
+  var newItemValue = newItemElement.value.trim();
+
+  if (!newItemValue) return;
+
+  listItems.push(newItemValue);
+  newItemElement.value = "";
+
+  updateList(listItems);
+};
